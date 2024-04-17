d11a671c1dc9cdbea77b38a6e49ed6667b19a569
CaptionLuffy <159707662+CaptionLuffy@users.noreply.github.com>
2024-04-17 10:42:27 +0530

    Update link.py

--- plugins/link.py
+++ plugins/link.py
@@ -1,7 +1,7 @@
 from http.client import EXPECTATION_FAILED
 from aniko import Aniko
 from matplotlib.image import thumbnail
-from pyrogram import Client, filters
+from pyrogram import Client, filters, enums
 from pyrogram.types import (
     Message, InlineKeyboardMarkup,
     InlineKeyboardButton
@@ -48,7 +48,7 @@ async def dl(client: Client, message: Message):
                await client.send_message(
                    chat_id=message.chat.id,
                    text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/Bots_Universe).",
-                   parse_mode="markdown",
+                   parse_mode=enums.ParseMode.MARKDOWN,
                    disable_web_page_preview=True
                )
                return
@@ -63,14 +63,14 @@ async def dl(client: Client, message: Message):
                         ]
                     ]
                 ),
-                parse_mode="markdown"
+                parse_mode=enums.ParseMode.MARKDOWN
             )
             return
         except Exception as e:
             await client.send_message(
                 chat_id= message.chat.id,
                 text=f"Something went Wrong. Contact my [Support Group](https://t.me/Bots_Universe).\nError: {e}",
-                parse_mode="markdown",
+                parse_mode=enums.ParseMode.MARKDOWN,
                 disable_web_page_preview=True)
             return
 
